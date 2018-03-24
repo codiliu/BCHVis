bc1a69854fa2d4585d2adc5602672269e6f29258
liuqiangqiang <lqqyeah@gmail.com>
2018-03-25 02:32:21 +0800

    clean

--- src/components/rank.vue
+++ src/components/rank.vue
@@ -57,33 +57,106 @@ export default {
         processData[d['name']]['workload']+=d['workload']
       })
       
-      var aa=[]
+      var changeData=[]
+      console.log('processData:', processData)
       for(var index in processData){
-        aa.push(processData[index])
+        var record= processData[index];
+        record['processId'] = index
+        changeData.push(record)
       }
-      console.log('processData:', aa)
-      return aa
+      console.log('processData:', changeData)
+      return changeData
       
     },
     drawBarchart(data){
+
+      data.sort(function(a,b){
+        return b.workload-a.workload
+      })
       var containerWidth = +$('#rank-bar').width()
       var containerHeight = +$('#rank-bar').height()
 
-      var barWidth = containerHeight/(data.length*1.1)
-      var x = d3.scale.linear()
-          .domain([0, d3.max(data.map(function(d){return d['workload']}))])
-          .range([0, containerWidth]);
+      var margin = {top: 10, right: 20, bottom: 20, left: 20},
+          width = containerWidth - margin.left - margin.right,
+          height = containerHeight - margin.top - margin.bottom;
+      var barHeight = height/(data.length*1.1)
+      // Parse the date / time
+      var parseDate = d3.time.format("%Y-%m").parse;
 
-      console.log('containerHeight:', containerHeight)
-      console.log('barWidth:',barWidth)
+      var x = d3.scale.linear().range([0, width]);
 
-      d3.select("#rank-bar")
-        .selectAll("div")
-          .data(data)
-        .enter().append("div")
-          .style("width", function(d) { return x(d.workload) + "px"; })
-          .style("height", barWidth + "px")
-          .text(function(d) { return d.workload; });
+      var y = d3.scale.ordinal().rangePoints([0, height]);
+
+      var xAxis = d3.svg.axis()
+          .scale(x)
+          .orient("bottom")
+          .tickFormat(d3.time.format("%Y-%m"));
+
+      var yAxis = d3.svg.axis()
+          .scale(y)
+          .orient("left")
+          .ticks(10);
+
+      var rankSvg = d3.select("#rank-bar").append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+          .attr("transform", 
+                "translate(" + margin.left + "," + margin.top + ")");
+
+      x.domain([0, d3.max(data, function(d) { return d.workload; })]);
+      y.domain(data.map(function(d){ return d.processId}))
+
+      console.log('x:', x.domain())
+
+      rankSvg.append("g")
+           .attr("class", "y axis")
+           .call(yAxis)
+         .append("text")
+           .attr("transform", "rotate(-90)")
+           .attr("y", 6)
+           .attr("dy", ".71em")
+           .style("text-anchor", "end")
+           //.text("Value ($)");
+
+       var bars = rankSvg.selectAll("bar")
+           .data(data)
+         .enter().append("rect")
+           .style("fill", "steelblue")
+           .attr("y", function(d,i) { return y(d.processId)-barHeight/2; })
+           .attr("width", function(d){return x(d.workload)})
+           .attr("x", 0)
+           .attr("height", barHeight);
+
+       bars.append("text")
+                   .attr("class", "value")
+                   .attr("y", function(d,i) { return y(d.processId)-barHeight/2;})
+                   .attr("dx", 2) //margin right
+                   .attr("dy", ".35em") //vertical align middle
+                   .attr("text-anchor", "end")
+                   .text(function(d){
+                       return (d.workload);
+                   })
+                   .attr("x", function(d){
+                       var width = this.getBBox().width;
+                       return Math.max(width + 2, y(d.processId));
+                   });
+
+      // var barWidth = containerHeight/(data.length*1.1)
+      // var x = d3.scale.linear()
+      //     .domain([0, d3.max(data.map(function(d){return d['workload']}))])
+      //     .range([0, containerWidth]);
+
+      // console.log('containerHeight:', containerHeight)
+      // console.log('barWidth:',barWidth)
+
+      // d3.select("width")
+      //   .selectAll("div")
+      //     .data(data)
+      //   .enter().append("div")
+      //     .style("width", function(d) { return x(d.workload) + "px"; })
+      //     .style("height", barWidth + "px")
+      //     .text(function(d) { return d.workload; });
     }
   }
 }
